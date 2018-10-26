5398a812191265208350218deb85e50662040f40
ghidra <ghidra@gmail.com>
2018-10-26 00:00:35 -0400

    basic reloadable shader

--- SimpleShaderTest/resources/mandelbrot.frag
+++ SimpleShaderTest/resources/mandelbrot.frag
@@ -5,16 +5,16 @@
 // Sets the maximum number of iterations per pixel.
 // Note: anything above 256 is a waste of energy,
 //       because of the limited floating point precision.
-#define Iterations 6
+//#define Iterations 6
 
-uniform sampler2D uTex0;
+//uniform sampler2D uTex0;
 //uniform vec2      uCenter;
-uniform float     uAspectRatio;// = 1.33333;
+//uniform float     uAspectRatio;// = 1.33333;
 //uniform float     uScale = 1.0;
-uniform float uFreq = 1.0f;
-uniform float uTime;
-uniform float uPhase = 0.1f;
-uniform float uTestArray[Iterations];
+//uniform float uFreq = 1.0f;
+//uniform float uTime;
+//uniform float uPhase = 0.1f;
+//uniform float uTestArray[Iterations];
 
 in vec2 vertTexCoord0;
 
@@ -22,16 +22,16 @@ out vec4 fragColor;
 
 void main()
 {
-	vec2 cuv = ( vertTexCoord0 - vec2(0.5) ) * vec2(uAspectRatio,1.0);//make it "square" by strecting out x
-	float outside = 1.0;
-	for (int i=0; i<Iterations; i++)
-	{
-		float nx = SimplexPerlin3D( vec3( 0.0, 0.0, (float(i)*uFreq) + (uTime*uPhase) ) );
-		float ny = SimplexPerlin3D( vec3( 30.0, 192.4, ( float(i)*uFreq ) + (uTime*uPhase) ) );
-		vec2 center = vec2(nx,ny)*0.5;
-		float d = length( cuv - center );
-		outside *= min(d*12.0*uTestArray[i],1.0f);
-	}
+	// vec2 cuv = ( vertTexCoord0 - vec2(0.5) ) * vec2(uAspectRatio,1.0);//make it "square" by strecting out x
+	// float outside = 1.0;
+	// for (int i=0; i<Iterations; i++)
+	// {
+	// 	float nx = SimplexPerlin3D( vec3( 0.0, 0.0, (float(i)*uFreq) + (uTime*uPhase) ) );
+	// 	float ny = SimplexPerlin3D( vec3( 30.0, 192.4, ( float(i)*uFreq ) + (uTime*uPhase) ) );
+	// 	vec2 center = vec2(nx,ny)*0.5;
+	// 	float d = length( cuv - center );
+	// 	outside *= min(d*12.0*uTestArray[i],1.0f);
+	// }
 		
 
 	
@@ -40,5 +40,5 @@ void main()
 	//vec3 color = texture( uTex0, texCoord ).rgb;
 
 	// Output color.
-	fragColor = vec4( vec3(outside), 1.0 );
+	fragColor = vec4( vec3(vertTexCoord0,1.0), 1.0 );
 }
--- SimpleShaderTest/src/SimpleShaderTestApp.cpp
+++ SimpleShaderTest/src/SimpleShaderTestApp.cpp
@@ -6,6 +6,7 @@
 #include "cinder/ImageIo.h"
 
 #include "Resources.h"
+#include "Assets.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -20,8 +21,9 @@ public:
 	void draw() override;
 
 public:
-	gl::GlslProgRef		mBrotShader;
-	gl::Texture2dRef	mColorsTexture;
+	signals::ScopedConnection		mConnection;
+	gl::GlslProgRef		mShader;
+	//gl::Texture2dRef	mColorsTexture;
 
 	//vec2				mCenter;
 	float 				mLastTime = 0.0f;
@@ -38,22 +40,14 @@ void SimpleShaderTestApp::prepareSettings( Settings *settings )
 
 void SimpleShaderTestApp::setup()
 {
-	mTestArray[0] = 1.1f;
-	mTestArray[1] = 1.1f;
-	mTestArray[2] = 1.1f;
-	mTestArray[3] = 0.6f;
-	mTestArray[4] = 1.1f;
-	mTestArray[5] = 1.1f;
-	//! Zoom in on this location in the MandelBrot set.
-	//mCenter = vec2( -0.5430f, 0.53398f );
-	
-	//! Find other cool locations on: http://www.eddaardvark.co.uk/mandelbrot/webgl.html
-	//mCenter = vec2( -0.74699f, 0.08762f );
-	//mCenter = vec2( -0.922332f, 0.309948f );
-
+	 mConnection = assets()->getShader( "mandelbrot.vert", "mandelbrot.frag",
+                                      [this]( gl::GlslProgRef glsl ) {
+                                          mShader = glsl;
+										  //mShader->uniform( "uResolution", vec2((float)app::getWindowSize().x, (float)app::getWindowSize().y));
+                                      } );
 	//! Load shader and texture from the resources.
-	mBrotShader = gl::GlslProg::create( loadResource( RES_VERT_GLSL ), loadResource( RES_FRAG_GLSL ) );
-	mColorsTexture = gl::Texture::create( loadImage( loadResource( RES_COLORS_PNG ) ) );
+	//mShader = gl::GlslProg::create( loadResource( RES_VERT_GLSL ), loadResource( RES_FRAG_GLSL ) );
+	//mColorsTexture = gl::Texture::create( loadImage( loadResource( RES_COLORS_PNG ) ) );
 }
 
 //void SimpleShaderTestApp:update()
@@ -81,13 +75,14 @@ void SimpleShaderTestApp::draw()
 
 	//! Render the MandelBrot set by running the shader
 	//! for every pixel in the window.
-	gl::ScopedGlslProg glslScp( mBrotShader );
-	gl::ScopedTextureBind texScp( mColorsTexture );
-	mBrotShader->uniform( "uTex0", 0 );
-	mBrotShader->uniform( "uTime", mElapsedTime );
-	mBrotShader->uniform( "uTestArray", mTestArray, sizeof(mTestArray)/sizeof(mTestArray[0]) );
+	gl::ScopedGlslProg glslScp( mShader );
+	//gl::ScopedTextureBind texScp( mColorsTexture );
+	
+	//mShader->uniform( "uTex0", 0 );
+	mShader->uniform( "uTime", mElapsedTime );
+	//mShader->uniform( "uTestArray", mTestArray, sizeof(mTestArray)/sizeof(mTestArray[0]) );
 	//mBrotShader->uniform( "uScale", scale );
-	mBrotShader->uniform( "uAspectRatio", getWindowAspectRatio() );
+	mShader->uniform( "uAspectRatio", getWindowAspectRatio() );
 	gl::drawSolidRect( getWindowBounds() );
 }
 
